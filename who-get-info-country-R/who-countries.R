2995811c17f1b1308766e47ac2bf2f6c77ddb756
StayerX <yotov.st@gmail.com>
2015-11-22 00:22:29 -0600

    Working DB

--- who-get-info-country-R/who-countries.R
+++ who-get-info-country-R/who-countries.R
@@ -64,7 +64,7 @@ df[,1:2]<-countries
 for (code in countries$code){
   url<-paste("http://www.who.int/countries/",tolower(code),"/en/" ,sep="")
   validPage<-TRUE
-  tryCatch(thepage <- readLines(url),  error = function(e) validPage<-FALSE , finally = print("Hello"))
+  tryCatch(thepage <- readLines(url), warning = function(w) validPage<-FALSE,  error = function(e) validPage<-FALSE , finally = NULL)
   extracted_vals<-list()
   if(validPage){
     extracted_vals<-NULL
@@ -82,6 +82,7 @@ for (code in countries$code){
     # Do Nothing
   }
 }
+
 # Remove old Data Table
 if(dbExistsTable(con,tableName)) {dbRemoveTable(con,tableName)}
 # Finally write a new table:
